33aa50ecb340f0fa6b0e10127276812764a13809
Mikkel Mogensen <0x16e8bea@gmail.com>
2018-08-18 17:57:39 +0200

    Move ParticleController.cs and MinMax.cs
    Moved scripts to the correct folders.

---
+++ Assets/Code/Actors/Boids/ParticleController.cs
@@ -0,0 +1,56 @@
+﻿using Code.Actors.Boids;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+    private Renderer objRenderer;
+
+    [SerializeField] private AnimationCurve _nearDistCurve;
+    [SerializeField] private AnimationCurve _attractionCurve;
+    [SerializeField] private AnimationCurve _triggerDistCurve;
+
+    [SerializeField] private FlockSimulation _flockSimulation;
+
+    [SerializeField] private float _maxNeighbourDistance;
+    [SerializeField] private float _minNeighbourDistance;
+
+    [SerializeField] private float _maxAttractionForce;
+    [SerializeField] private float _minAttractionForce;
+
+    [SerializeField] private float _maxTriggerDistance;
+    [SerializeField] private float _minTriggerDistance;
+
+
+    // Use this for initialization
+    private void Start() {
+        SendPerformanceData.FlowChangeDelegate += ChangeTriggerDist;
+        SendPerformanceData.FlowChangeDelegate += ChangeAttraction;
+
+        SendPerformanceData.BodyVolumeDelegate += ChangeNearDist;
+        objRenderer = GetComponent<Renderer>();
+    }
+
+    private void ChangeNearDist(float dist) {
+        _flockSimulation.NeighbourDistance = _minNeighbourDistance +
+                                             _nearDistCurve.Evaluate(dist) *
+                                             (_maxNeighbourDistance - _minNeighbourDistance);
+    }
+
+    private void ChangeAttraction(float attraction) {
+        _flockSimulation.AttractionForce = _minAttractionForce +
+                                           _attractionCurve.Evaluate(attraction) *
+                                           (_maxAttractionForce - _minAttractionForce);
+    }
+
+    private void ChangeTriggerDist(float dist) {
+        _flockSimulation.TriggerDistance = _minTriggerDistance +
+                                           _triggerDistCurve.Evaluate(dist) *
+                                           (_maxTriggerDistance - _minTriggerDistance);
+    }
+
+    // Unsubscribing Delegate
+    private void OnDisable() {
+        SendPerformanceData.FlowChangeDelegate -= ChangeTriggerDist;
+        SendPerformanceData.FlowChangeDelegate -= ChangeAttraction;
+        SendPerformanceData.BodyVolumeDelegate -= ChangeNearDist;
+    }    
+}
---
+++ Assets/Code/Misc/MinMax.cs
@@ -0,0 +1,90 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+public class MinMax : MonoBehaviour {
+	[Title("$MyTitle")] public bool HideProperties;
+
+	[OdinSerialize, HideIf("HideProperties")]
+	public string MyTitle;
+
+
+	[SerializeField] private float _minFloat;
+	[SerializeField] private float _maxFloat;
+
+	public float MinFloat {
+		get { return _minFloat; }
+		set { _minFloat = value; }
+	}
+
+	public float MaxFloat {
+		get { return _maxFloat; }
+		set { _maxFloat = value; }
+	}
+
+
+	public void CalculateMaxMinFloat(float val) {
+		_minFloat = Mathf.Min(_minFloat, val);
+		_maxFloat = Mathf.Max(_maxFloat, val);
+	}
+
+	public void GetMaxMinFloat(out float min, out float max) {
+		min = _minFloat;
+		max = _maxFloat;
+	}
+
+	[SerializeField] private Vector2 _minVec2;
+	[SerializeField] private Vector2 _maxVec2;
+
+	public Vector2 MinVec2 {
+		get { return _minVec2; }
+		set { _minVec2 = value; }
+	}
+
+	public Vector2 MaxVec2 {
+		get { return _maxVec2; }
+		set { _maxVec2 = value; }
+	}
+
+	public void CalculateMaxMinVec2(Vector2 vec) {
+		_minVec2.x = Mathf.Min(_minVec2.x, vec.x);
+		_minVec2.y = Mathf.Min(_minVec2.x, vec.x);
+
+		_maxVec2.x = Mathf.Max(_maxVec2.x, vec.x);
+		_maxVec2.y = Mathf.Max(_maxVec2.y, vec.y);
+	}
+
+
+	public void GetMaxMinVec2(out Vector2 min, out Vector2 max) {
+		min = _minVec2;
+		max = _maxVec2;
+	}
+
+	[SerializeField] private Vector3 _minVec3;
+	[SerializeField] private Vector3 _maxVec3;
+
+	public Vector3 MinVec3 {
+		get { return _minVec3; }
+		set { _minVec3 = value; }
+	}
+
+	public Vector3 MaxVec3 {
+		get { return _maxVec3; }
+		set { _maxVec3 = value; }
+	}
+
+	public void CalculateMaxMinVec3(Vector3 vec) {
+		_minVec3.x = Mathf.Min(_minVec3.x, vec.x);
+		_minVec3.y = Mathf.Min(_minVec3.x, vec.x);
+		_minVec3.z = Mathf.Min(_minVec3.x, vec.x);
+
+		_maxVec3.x = Mathf.Max(_maxVec3.x, vec.x);
+		_maxVec3.y = Mathf.Max(_maxVec3.y, vec.y);
+		_maxVec3.z = Mathf.Max(_maxVec3.z, vec.z);
+	}
+
+	public void GetMaxMinVec3(out Vector3 min, out Vector3 max) {
+		min = _minVec3;
+		max = _maxVec3;
+	}
+}
